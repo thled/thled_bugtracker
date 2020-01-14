05aa788e3ca3c63cdd40a07bb56745b36bfeb634
Thomas Le Duc <dev@tleduc.de>
2020-01-14 08:18:41 +0100

    Add a test controller

---
+++ app/src/Controller/TestController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test")
+     */
+    public function testAction(): Response
+    {
+        return new Response('foobar');
+    }
+}
